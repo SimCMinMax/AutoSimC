d0fa995a40edf60c0edef237072788396417385a
Julien Rechaussat <julien.rechaussat@free.fr>
2017-03-20 13:14:45 +0100

    Better default batch

--- launch.bat
+++ launch.bat
@@ -1,1 +1,4 @@
-python main.py settings.ini out.simc "head|id|bonus|enchant|gem,shoulders|id2|bonus2|enchant2|gem2" 2:2
+python main.py settings.ini out.simc
+@echo off
+REM For legendary permutations in command line, use this command :
+REM python main.py settings.ini out.simc "leg1|id|bonus|enchant|gem,leg2|id2|bonus2|enchant2|gem2" 2:2
