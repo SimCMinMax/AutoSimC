f233822f6385f19ab2ae7790f84a1cd971d1898d
theoneandlonely <dabulla@gmail.com>
2017-06-01 16:01:36 +0200

    check for python.exe in path before launch

--- launch.bat
+++ launch.bat
@@ -1,5 +1,13 @@
-python.exe main.py -i input.txt -o out.simc -quiet -sim stage1
-pause
 @echo off
+where /q python.exe
+IF ERRORLEVEL 1 (
+   ECHO Python.exe is missing. Ensure it is installed and placed in your PATH.
+   timeout 10
+   EXIT /B
+) ELSE (
+    ECHO Python.exe exists. Launching AutoSimC!
+   python.exe main.py -i input.txt -o out.simc -quiet -sim stage1
+)
+
 REM For legendary permutations in command line, use this command :
 REM python main.py -i settings.ini -o out.simc -l "leg1|id|bonus|enchant|gem,leg2|id2|bonus2|enchant2|gem2" 0:2
