14a09a597eb1e97756f820244cab13085182663b
theoneandlonely <dabulla@gmail.com>
2017-09-01 22:51:21 +0200

    changed profile-naming to include legendaries and type of tier-boni

--- main.py
+++ main.py
@@ -121,6 +121,10 @@ def handleGems(gems):
 
 # Check if permutation is valid
 def checkUsability():
+    # namingdata contains info for the profile-name
+    global namingData
+    namingData = {}
+
     temp_t19 = 0
     temp_t20 = 0
     temp_t21 = 0
@@ -162,7 +166,33 @@ def checkUsability():
             nUniqueGems += 1
     if nUniqueGems > 1:
         return str(nUniqueGems) + " too many unique gems (str, agi, int)"
+
+    # if a valid profile was detected, fill namingData; otherwise its pointless
+    if nbLeg == 0:
+        namingData['Leg0'] = ""
+        namingData["Leg1"] = ""
+    elif nbLeg == 1:
+        for a in range(len(l_gear)):
+            if l_gear[a][0] == "L":
+                namingData['Leg0'] = getIdFromItem(l_gear[a][0])
+    elif nbLeg == 2:
+        for a in range(len(l_gear)):
+            if l_gear[a][0] == "L":
+                if namingData.get('Leg0') != None:
+                    namingData['Leg1'] = getIdFromItem(l_gear[a])
+                else:
+                    namingData['Leg0'] = getIdFromItem(l_gear[a])
+    namingData["T19"] = temp_t19
+    namingData["T20"] = temp_t20
+    namingData["T21"] = temp_t21
     return ""
+
+
+def getIdFromItem(item):
+    splits = item.split(",")
+    for s in splits:
+        if s.startswith("id="):
+            return s[3:]
 
 
 # Print a simc profile
@@ -186,7 +216,7 @@ def scpout(oh):
     else:
         if not b_quiet:
             print("Profile:" + str(maskedProfileID) + "/" + str(c_profilemaxid))
-        outputFile.write(c_class + "=" + c_profilename + "_" + maskedProfileID + "\n")
+        outputFile.write(c_class + "=" + getStringForProfile() + maskedProfileID + "\n")
         outputFile.write("specialization=" + c_spec + "\n")
         outputFile.write("race=" + c_race + "\n")
         outputFile.write("level=" + c_level + "\n")
@@ -194,7 +224,7 @@ def scpout(oh):
         outputFile.write("position=" + c_position + "\n")
         outputFile.write("talents=" + c_talents + "\n")
         outputFile.write("artifact=" + c_artifact + "\n")
-        outputFile.write("crucible=" + c_crucible + "\n")
+        # outputFile.write("crucible=" + c_crucible + "\n")
         if c_other != "":
             outputFile.write(c_other + "\n")
         if l_gear[0][0] == "L":
@@ -282,7 +312,7 @@ def scpoutprofileset(oh):
             print("Profile:" + str(maskedProfileID) + "/" + str(c_profilemaxid))
         # generate first profile
         if i_generatedProfiles == 0:
-            outputFile.write(c_class + "=" + c_profilename + "_" + maskedProfileID + "\n")
+            outputFile.write(c_class + "=" + getStringForProfile() + maskedProfileID + "\n")
             outputFile.write("specialization=" + c_spec + "\n")
             outputFile.write("race=" + c_race + "\n")
             outputFile.write("level=" + c_level + "\n")
@@ -577,7 +607,7 @@ def validateSettings():
     # validate tier-set
     if (int(t19) + int(t20) + int(t21) > 6) or t19 < 0 or t19 > 6 or t20 < 0 or t20 > 6 or t21 < 0 or t21 > 6:
         printLog("Error: Wrong Tier-Set-Combination: T19: " + str(t19) + ", T20: " + str(
-            t20) + ", T21: " + str(t21) +". Please check settings.py for these parameters!")
+            t20) + ", T21: " + str(t21) + ". Please check settings.py for these parameters!")
         sys.exit(1)
     # use a "safe mode", overwriting the values
     if settings.simc_safe_mode:
@@ -725,7 +755,8 @@ def permutateGems():
     permutateGemsInSlotGearList(l_trinket1, 12)
     permutateGemsInSlotGearList(l_trinket2, 13)
 
-#todo: add checks for missing headers, prio low
+
+# todo: add checks for missing headers, prio low
 def permutate():
     # Read input.txt to init vars
     config = configparser.ConfigParser()
@@ -756,7 +787,7 @@ def permutate():
     global c_artifact
     c_artifact = profile['artifact']
     global c_crucible
-    c_crucible = profile['crucible']
+    # c_crucible = profile['crucible']
     global c_other
     c_other = profile['other']
 
@@ -868,7 +899,6 @@ def permutate():
         for t in temp_talents:
             if t not in l_talents:
                 l_talents.append(t)
-
 
     # add gem-permutations
     if gemspermutation:
@@ -1368,7 +1398,513 @@ def stage3_restart():
         printLog("Error, wrong mode")
 
 
-#########################
+def getAcronymForID(id):
+    # shared
+    if id == "133976":
+        return "Cind"
+    elif id == "137015":
+        return "Eko"
+    elif id == "146669":
+        return "Sentinel"
+    elif id == "132455":
+        return "Norgannon"
+    elif id == "146666":
+        return "Celumbra"
+    elif id == "132466":
+        return "Roots"
+    elif id == "146668":
+        return "Perch"
+    elif id == "132443":
+        return "Aggramar"
+    elif id == "146667":
+        return "Rethus"
+    elif id == "132444":
+        return "Prydaz"
+    elif id == "152626":
+        return "Insignia"
+    elif id == "132452":
+        return "Sephuz"
+    elif id == "144249":
+        return "AHR"
+    elif id == "144258":
+        return "Velen"
+    elif id == "144259":
+        return "KJ"
+
+    # demonhunter
+    elif id == "137014":
+        return "Achor"
+    elif id == "137022":
+        return "Lor"
+    elif id == "137061":
+        return "Raddon"
+    elif id == "137071":
+        return "Rune"
+    elif id == "137090":
+        return "Bio"
+    elif id == "137091":
+        return "Defile"
+    elif id == "138949":
+        return "Kirel"
+    elif id == "144279":
+        return "Grandeur"
+    elif id == "144292":
+        return "Spirit"
+    elif id == "151799":
+        return "Oblivion"
+    elif id == "137038":
+        return "Anger"
+    elif id == "138854":
+        return "Fragment"
+    elif id == "151639":
+        return "Soul"
+
+    # druid
+    elif id == "137023":
+        return "POE"
+    elif id == "137024":
+        return "Fel"
+    elif id == "137026":
+        return "EOI"
+    elif id == "137025":
+        return "Sky"
+    elif id == "137056":
+        return "Luffa"
+    elif id == "137062":
+        return "ED"
+    elif id == "137067":
+        return "Elize"
+    elif id == "137072":
+        return "Aman"
+    elif id == "137078":
+        return "Titan"
+    elif id == "137092":
+        return "Oneth"
+    elif id == "137095":
+        return "Edraith"
+    elif id == "137096":
+        return "Petri"
+    elif id == "137094":
+        return "Wild"
+    elif id == "144242":
+        return "Xonis"
+    elif id == "144295":
+        return "LATC"
+    elif id == "144354":
+        return "Fiery"
+    elif id == "144432":
+        return "Oak"
+    elif id == "151783":
+        return "Karma"
+    elif id == "151801":
+        return "Behemoth"
+    elif id == "137039":
+        return "IFE"
+    elif id == "137040":
+        return "Chatoyant"
+    elif id == "137041":
+        return "Dual"
+    elif id == "137042":
+        return "Tearstone"
+    elif id == "151636":
+        return "Soul"
+
+    # monk
+    elif id == "137016":
+        return "Sal"
+    elif id == "137027":
+        return "Fire"
+    elif id == "137028":
+        return "Eithas"
+    elif id == "137029":
+        return "Kat"
+    elif id == "137057":
+        return "Touch"
+    elif id == "137063":
+        return "FO"
+    elif id == "137068":
+        return "Flame"
+    elif id == "137073":
+        return "Unison"
+    elif id == "137079":
+        return "Gai"
+    elif id == "137097":
+        return "Horn"
+    elif id == "138879":
+        return "Ovyds"
+    elif id == "144239":
+        return "Capa"
+    elif id == "144277":
+        return "Anvil"
+    elif id == "144340":
+        return "Rins"
+    elif id == "151788":
+        return "Stout"
+    elif id == "151811":
+        return "Wind"
+    elif id == "137044":
+        return "Jewel"
+    elif id == "137045":
+        return "Eye"
+    elif id == "137220":
+        return "March"
+    elif id == "151643":
+        return "Soul"
+
+    # rogue
+    elif id == "137030":
+        return "Dusk"
+    elif id == "137031":
+        return "Thrax"
+    elif id == "137032":
+        return "Satyr"
+    elif id == "137069":
+        return "Val"
+    elif id == "137098":
+        return "Zoldyck"
+    elif id == "137099":
+        return "Greenskin"
+    elif id == "137100":
+        return "Giants"
+    elif id == "141321":
+        return "Shiv"
+    elif id == "144236":
+        return "Cloak"
+    elif id == "151815":
+        return "Crown"
+    elif id == "151818":
+        return "Dead"
+    elif id == "137049":
+        return "Insignia"
+    elif id == "150936":
+        return "Soul"
+
+    # mage
+    elif id == "132406":
+        return "Sun"
+    elif id == "132411":
+        return "Vashj"
+    elif id == "132413":
+        return "Rhonin"
+    elif id == "132442":
+        return "Cord"
+    elif id == "132451":
+        return "Kilt"
+    elif id == "132454":
+        return "Koralon"
+    elif id == "132863":
+        return "Darcklis"
+    elif id == "133970":
+        return "Zannesu"
+    elif id == "133977":
+        return "Belovir"
+    elif id == "138140":
+        return "Mag"
+    elif id == "144260":
+        return "Ice"
+    elif id == "144274":
+        return "Gravity"
+    elif id == "144355":
+        return "Pyrotex"
+    elif id == "151808":
+        return "Kirin"
+    elif id == "151809":
+        return "Core"
+    elif id == "151810":
+        return "Sindra"
+    elif id == "132410":
+        return "Shard"
+    elif id == "151642":
+        return "Soul"
+
+    # priest
+    elif id == "132409":
+        return "Anunds"
+    elif id == "132436":
+        return "Skjoldr"
+    elif id == "132437":
+        return "Sharaz"
+    elif id == "132445":
+        return "Almaiesh"
+    elif id == "132447":
+        return "Anjuna"
+    elif id == "132450":
+        return "Muzes"
+    elif id == "132461":
+        return "Xalan"
+    elif id == "132861":
+        return "Estel"
+    elif id == "132864":
+        return "Mangazas"
+    elif id == "133800":
+        return "Maiev"
+    elif id == "133971":
+        return "Zenkaram"
+    elif id == "144244":
+        return "Xiraff"
+    elif id == "144247":
+        return "Rammal"
+    elif id == "151786":
+        return "Hallation"
+    elif id == "151814":
+        return "Heart"
+    elif id == "151787":
+        return "Lady"
+    elif id == "132449":
+        return "Phyrix"
+    elif id == "133973":
+        return "Twins"
+    elif id == "151646":
+        return "Soul"
+    elif id == "137276":
+        return "Nero"
+
+    # warlock
+    elif id == "132357":
+        return "Pillar"
+    elif id == "132374":
+        return "Kazzak"
+    elif id == "132379":
+        return "Sindorei"
+    elif id == "132381":
+        return "Stretens"
+    elif id == "132393":
+        return "Ritual"
+    elif id == "132394":
+        return "Hood"
+    elif id == "132407":
+        return "Magi"
+    elif id == "132456":
+        return "FoS"
+    elif id == "132457":
+        return "Cord"
+    elif id == "144369":
+        return "Lost"
+    elif id == "144385":
+        return "Wakener"
+    elif id == "151821":
+        return "Harvest"
+    elif id == "132369":
+        return "Wilfred"
+    elif id == "132378":
+        return "Sacro"
+    elif id == "132460":
+        return "Alythess"
+    elif id == "151649":
+        return "Soul"
+
+
+    # hunter
+    elif id == "137033":
+        return "Ullr"
+    elif id == "137034":
+        return "Nesing"
+    elif id == "137064":
+        return "Voodoo"
+    elif id == "137064":
+        return "Roar"
+    elif id == "137081":
+        return "Sentinel"
+    elif id == "137082":
+        return "Helbrine"
+    elif id == "137101":
+        return "Call"
+    elif id == "137227":
+        return "Qapla"
+    elif id == "141353":
+        return "Cap"
+    elif id == "144303":
+        return "Gyro"
+    elif id == "144326":
+        return "Mantle"
+    elif id == "144361":
+        return "Butcher"
+    elif id == "151805":
+        return "Parsel"
+    elif id == "137043":
+        return "Frizzo"
+    elif id == "137055":
+        return "Zevrim"
+    elif id == "137382":
+        return "Apex"
+    elif id == "151641":
+        return "Soul"
+
+    # shaman
+    elif id == "137058":
+        return "Tide"
+    elif id == "137074":
+        return "Echoes"
+    elif id == "137083":
+        return "Girdle"
+    elif id == "137084":
+        return "Akainus"
+    elif id == "137085":
+        return "Nazjatar"
+    elif id == "137102":
+        return "AlAkir"
+    elif id == "137103":
+        return "Tempest"
+    elif id == "137104":
+        return "Nobundo"
+    elif id == "137616":
+        return "Emalon"
+    elif id == "138117":
+        return "Journey"
+    elif id == "143732":
+        return "Reminder"
+    elif id == "151785":
+        return "Deep"
+    elif id == "151819":
+        return "Heart"
+    elif id == "137035":
+        return "Deceiver"
+    elif id == "137036":
+        return "Rebalancer"
+    elif id == "137050":
+        return "Eye"
+    elif id == "137051":
+        return "Jonat"
+    elif id == "151647":
+        return "Soul"
+
+
+    # dk
+    elif id == "132365":
+        return "Bryndaor"
+    elif id == "132366":
+        return "Koltiras"
+    elif id == "132367":
+        return "Gorefiend"
+    elif id == "132441":
+        return "Draugr"
+    elif id == "132448":
+        return "Fourth"
+    elif id == "132453":
+        return "Rattlegore"
+    elif id == "132458":
+        return "Toravon"
+    elif id == "132459":
+        return "Martyr"
+    elif id == "137075":
+        return "Taktheritrix"
+    elif id == "144280":
+        return "March"
+    elif id == "144281":
+        return "Skull"
+    elif id == "144293":
+        return "CCC"
+    elif id == "151795":
+        return "Soulflayer"
+    elif id == "151796":
+        return "Heart"
+    elif id == "133974":
+        return "Lament"
+    elif id == "137037":
+        return "Uvanimor"
+    elif id == "137223":
+        return "Necro"
+    elif id == "151640":
+        return "Deathlord"
+
+    # warrior
+    elif id == "137060":
+        return "Archavon"
+    elif id == "137077":
+        return "Weight"
+    elif id == "137087":
+        return "Najentus"
+    elif id == "137088":
+        return "Ceann"
+    elif id == "137089":
+        return "Vigor"
+    elif id == "137107":
+        return "Mannoroth"
+    elif id == "137108":
+        return "Kakushan"
+    elif id == "143728":
+        return "Strata"
+    elif id == "151822":
+        return "Ararat"
+    elif id == "151823":
+        return "Eye"
+    elif id == "151824":
+        return "Berserker"
+    elif id == "137052":
+        return "Ayala"
+    elif id == "137054":
+        return "Walls"
+    elif id == "151650":
+        return "Soul"
+
+    # paladin
+    elif id == "137017":
+        return "Valkyr"
+    elif id == "137059":
+        return "Tyrs"
+    elif id == "137065":
+        return "Gaze"
+    elif id == "137070":
+        return "Tyelca"
+    elif id == "137076":
+        return "Obsidian"
+    elif id == "137086":
+        return "Thrayns"
+    elif id == "137105":
+        return "Uther"
+    elif id == "140846":
+        return "Aegisjalmur"
+    elif id == "144275":
+        return "Saruan"
+    elif id == "144358":
+        return "Ashes"
+    elif id == "151782":
+        return "Tower"
+    elif id == "151812":
+        return "Pillars"
+    elif id == "151813":
+        return "Inquisitor"
+    elif id == "137046":
+        return "Ilterendi"
+    elif id == "137047":
+        return "Heathcliff"
+    elif id == "137048":
+        return "Liadrin"
+    elif id == "151644":
+        return "Soul"
+
+
+def getStringForProfile():
+    # example: "Uther_Soul_T19-2p_T20-2p_T21-2p"
+    # scpout later adds a increment for multiple versions of this
+    template = "%A%B%C%D%E"
+    if namingData.get('Leg0') != "None":
+        template = template.replace("%A", str(getAcronymForID(namingData.get('Leg0'))) + "_")
+    else:
+        template = template.replace("%A", "")
+    if namingData.get('Leg1') != "None":
+        template = template.replace("%B", str(getAcronymForID(namingData.get('Leg1'))) + "_")
+    else:
+        template = template.replace("%B", "")
+    if namingData.get("T19") != "None" and namingData.get("T19") != 0 and namingData.get(
+            "T19") != 1 and namingData.get("T19") != 3 and namingData.get("T19") != 5:
+        template = template.replace("%C", "T19-" + str(namingData.get('T19')) + "p_")
+    else:
+        template = template.replace("%C", "")
+    if namingData.get("T20") != "None" and namingData.get("T20") != 0 and namingData.get(
+            "T20") != 1 and namingData.get("T20") != 3 and namingData.get("T20") != 5:
+        template = template.replace("%D", "T20-" + str(namingData.get('T20')) + "p_")
+    else:
+        template = template.replace("%D", "")
+    if namingData.get("T21") != "None" and namingData.get("T21") != 0 and namingData.get(
+            "T21") != 1 and namingData.get("T21") != 3 and namingData.get("T21") != 5:
+        template = template.replace("%E", "T21-" + str(namingData.get('T21')) + "p_")
+    else:
+        template = template.replace("%E", "")
+    return template
+
+
+########################
 #### Program Start ######
 #########################
 sys.stderr = open(errorFileName, 'w')
