fd6f27b9e403341fab00e255936b9a415b89d4cc
Serge Camille <sergecamille@gmail.com>
2017-11-15 10:35:56 +0100

    Calculate bandwith with microseconds still available in elapsed time.

--- main.py
+++ main.py
@@ -500,11 +500,11 @@ def print_permutation_progress(current, maximum, start_time, max_profile_chars):
     if current % 50000 == 0 or current == maximum:
         pct = 100.0 * current / maximum
         elapsed = datetime.datetime.now() - start_time
+        bandwith = current / 1000 / elapsed.total_seconds() if elapsed.total_seconds() else 0.0
         elapsed = chop_microseconds(elapsed)
         remaining_time = elapsed * (100.0 / pct - 1.0) if current else "nan"
         if type(remaining_time) is datetime.timedelta:
             remaining_time = chop_microseconds(remaining_time)
-        bandwith = current / 1000 / elapsed.total_seconds() if elapsed.total_seconds() else 0.0
         logging.info("Processed {}/{} ({:5.2f}%) elapsed_time {} remaining {} bandwith {:.0f}k/s".
                      format(str(current).rjust(max_profile_chars),
                             maximum,
