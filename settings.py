47a44a8ac925d6b7ef835358f09e2bd012ece9a1
Serge Camille <sergecamille@gmail.com>
2017-12-10 19:57:19 +0100

    Make simc_path a raw string literal, so we can use single backslashes.

--- settings.py
+++ settings.py
@@ -5,8 +5,8 @@ class settings():
     # Path to your SimulationCraft command line binary (simc.exe on Windows, or simc on linux/mac).
     # If you enable the simulation-part, you need to either set simc_path, or enable auto_download on Windows.
     # Don´t point to the gui-executable. If a window with buttons and tabs opens, you chose the wrong executable!
-    # Either use forward slashes, or >>>>DOUBLE-BACKSLASH<<<< for subfolders.
-    simc_path = 'D:/simc-730-03-win64/simc.exe'
+    # Either use forward slashes, or >>>>SINGLE-BACKSLASH<<<< for subfolders. Do not remove the leading r'
+    simc_path = r'D:\simc-730-03-win64\simc.exe'
 
     # On Windows, AutoSimCor can automatically download the latest nightly version of SimulationCraft for you.
     # You need 7z command line utility in path to unzip for this to work.
