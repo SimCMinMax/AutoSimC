397b714ff71844d1a480f5515b06d1921a5a6213
Michael Farrell <micolous+git@gmail.com>
2021-10-10 10:20:30 +1100

    Add tests for QT5 split

--- tests/test_simc.py
+++ tests/test_simc.py
@@ -7,9 +7,11 @@ from unittest.mock import patch
 import simc
 from simc import SimcVersion
 
-with open(os.path.join(os.path.dirname(__file__), 'data', 'downloads.html'),
-          'rb') as f:
+_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+with open(os.path.join(_DATA_DIR, 'downloads.html'), 'rb') as f:
     _DOWNLOADS_PAGE = f.read()
+with open(os.path.join(_DATA_DIR, 'downloads_qt5.html'), 'rb') as f:
+    _DOWNLOADS_QT5_PAGE = f.read()
 
 
 class SimcTest(unittest.TestCase):
@@ -106,5 +108,68 @@ class SimcTest(unittest.TestCase):
         with patch.object(simc,
                           'urlopen',
                           return_value=BytesIO(_DOWNLOADS_PAGE)):
+            self.assertIsNone(
+                simc.latest_simc_version(major_ver='810-01', platform='win64'))
+
+    def test_get_version_qt5(self):
+        with patch.object(simc,
+                          'urlopen',
+                          return_value=BytesIO(_DOWNLOADS_QT5_PAGE)) as m:
+            self.assertEqual(simc.latest_simc_version(platform='win64'),
+                             ('simc-910-01-win64-ff51bf3.7z', 'ff51bf3'))
+        m.assert_called_once_with(
+            'http://downloads.simulationcraft.org/nightly/?C=M;O=D')
+
+        with patch.object(simc,
+                          'urlopen',
+                          return_value=BytesIO(_DOWNLOADS_QT5_PAGE)):
+            self.assertEqual(simc.latest_simc_version(platform='winarm64'),
+                             ('simc-910-01-winarm64-ff51bf3.7z', 'ff51bf3'))
+
+        with patch.object(simc,
+                          'urlopen',
+                          return_value=BytesIO(_DOWNLOADS_QT5_PAGE)):
+            self.assertEqual(simc.latest_simc_version(platform='win32_qt5'),
+                             ('simc-910-01-win32_qt5-ff51bf3.7z', 'ff51bf3'))
+
+        with patch.object(simc,
+                          'urlopen',
+                          return_value=BytesIO(_DOWNLOADS_QT5_PAGE)):
+            self.assertEqual(simc.latest_simc_version(platform='win64_qt5'),
+                             ('simc-910-01-win64_qt5-ff51bf3.7z', 'ff51bf3'))
+
+        with patch.object(simc,
+                          'urlopen',
+                          return_value=BytesIO(_DOWNLOADS_QT5_PAGE)):
+            self.assertEqual(simc.latest_simc_version(platform='macos'),
+                             ('simc-910-01-macos-ff51bf3.dmg', 'ff51bf3'))
+
+        # Platform auto-detection
+        with (patch.object(simc, 'simc_platform', return_value='macos'),
+              patch.object(simc,
+                           'urlopen',
+                           return_value=BytesIO(_DOWNLOADS_QT5_PAGE))):
+            self.assertEqual(simc.latest_simc_version(),
+                             ('simc-910-01-macos-ff51bf3.dmg', 'ff51bf3'))
+
+        # Platform auto-detection failure
+        with (patch.object(simc, 'simc_platform', return_value=None),
+              patch.object(simc,
+                           'urlopen',
+                           return_value=BytesIO(_DOWNLOADS_QT5_PAGE))):
+            self.assertIsNone(simc.latest_simc_version())
+
+        # Other versions
+        with patch.object(simc,
+                          'urlopen',
+                          return_value=BytesIO(_DOWNLOADS_QT5_PAGE)):
+            self.assertEqual(
+                simc.latest_simc_version(major_ver='830-02', platform='win64'),
+                ('simc-830-02-win64-8790a08.7z', '8790a08'))
+
+        # No match
+        with patch.object(simc,
+                          'urlopen',
+                          return_value=BytesIO(_DOWNLOADS_QT5_PAGE)):
             self.assertIsNone(
                 simc.latest_simc_version(major_ver='810-01', platform='win64'))
